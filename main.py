cc6b52f9812f095597d75fcc94f910a796e3c39d
Dipuuu111 <88713641+Dipuuu111@users.noreply.github.com>
2024-07-08 01:51:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -112,6 +112,9 @@ twitch_miner.mine(
    ],   
     [
        Streamer("top_drops", settings=StreamerSettings(make_predictions=True  , follow_raid=True , claim_drops=True  , watch_streak=True , bet=BetSettings(strategy=Strategy.SMART      , percentage=5 , stealth_mode=True,  percentage_gap=20 , max_points=234000 , filter_condition=FilterCondition(by=OutcomeKeys.TOTAL_USERS,      where=Condition.LTE, value=800 ) ) )),
+   ],
+    [
+       Streamer("gamegiveawaystv", settings=StreamerSettings(make_predictions=True  , follow_raid=True , claim_drops=True  , watch_streak=True , bet=BetSettings(strategy=Strategy.SMART      , percentage=5 , stealth_mode=True,  percentage_gap=20 , max_points=234000 , filter_condition=FilterCondition(by=OutcomeKeys.TOTAL_USERS,      where=Condition.LTE, value=800 ) ) )),
    ],# Array of streamers (order = priority)
     followers=False,                    # Automatic download the list of your followers
     followers_order=FollowersOrder.ASC  # Sort the followers list by follow date. ASC or DESC

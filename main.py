93e04ece18af00ef7a43cd1061289e99f952d145
Dipuuu111 <88713641+Dipuuu111@users.noreply.github.com>
2026-02-20 20:34:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -45,13 +45,6 @@ twitch_miner = TwitchChannelPointsMiner(
             streamer_offline="red",             # Read more in README.md
             BET_wiN=Fore.MAGENTA                # Color allowed are: [BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET].
         ),
-        telegram=Telegram(                                                          # You can omit or set to None if you don't want to receive updates on Telegram
-            chat_id=123456789,                                                      # Chat ID to send messages @getmyid_bot
-            token="123456789:shfuihreuifheuifhiu34578347",                          # Telegram API token @BotFather
-            events=[Events.STREAMER_ONLINE, Events.STREAMER_OFFLINE,
-                    Events.BET_LOSE, Events.CHAT_MENTION],                          # Only these events will be sent to the chat
-            disable_notification=True,                                              # Revoke the notification (sound/vibration)
-        ),
         discord=Discord(
             webhook_api="https://discord.com/api/webhooks/1378739038579720323/v-g_OmDODucmpA-zBjFzXufRhQtiQJQ8wy3RDg4isq0DGXlcy997S7_1Y-WyDYD7RsMD",  # Discord Webhook URL
             events=[Events.STREAMER_ONLINE, Events.STREAMER_OFFLINE,

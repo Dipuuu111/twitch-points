8f538e8e81e12d5c0d545b85342a3101687d792e
Diptanshu Pandey <88713641+Dipuuu111@users.noreply.github.com>
2026-05-09 21:22:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ import requests
 
 
 def auto_redeem_gokiccoon():
-    OAUTH_TOKEN       = os.environ.get("TWITCH_OAUTH_TOKEN")
+    OAUTH_TOKEN       = os.environ.get("TWITCH_OAUTH_TOKEN_REDEMPTION")
     CLIENT_ID         = os.environ.get("TWITCH_CLIENT_ID")
     DISCORD_WEBHOOK   = "https://discord.com/api/webhooks/1378739038579720323/v-g_OmDODucmpA-zBjFzXufRhQtiQJQ8wy3RDg4isq0DGXlcy997S7_1Y-WyDYD7RsMD"
 
